cad72f3b341981a57a277e3cd8828d36e676075d
Cjmars0912 <christianmarsh0912@gmail.com>
2023-04-13 19:04:09 -0400

    updates to the navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,83 +1,70 @@
-import { useRef, useEffect, useState } from 'react';
+import {
+  useRef,
+  useEffect,
+  useState,
+  MutableRefObject,
+  ChangeEvent,
+} from 'react';
 import { Link } from 'react-router-dom';
 import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io';
 import { AiOutlineClose, AiOutlineSearch } from 'react-icons/ai';
 import { CSSTransition } from 'react-transition-group';
 import styles from './navbar.module.css';
 
-function useOutsideAlerter(ref: any, test: any) {
-  useEffect(() => {
-    function handleClickOutside(event: any) {
-      if (ref.current && !ref.current.contains(event.target)) {
-        test();
-      }
+// custom hook for detecting when you click outside of an element and running a function as a result
+const useOutsideClick = (
+  ref: MutableRefObject<HTMLElement | null>,
+  callback: () => void
+) => {
+  const handleClick = (event: MouseEvent) => {
+    if (ref.current && !ref.current.contains(event.target as Node)) {
+      callback();
     }
-    // Bind the event listener
-    document.addEventListener('click', handleClickOutside);
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClick);
     return () => {
-      // Unbind the event listener on clean up
-      document.removeEventListener('click', handleClickOutside);
+      document.removeEventListener('mousedown', handleClick);
     };
-  }, [ref]);
-}
+  }, [ref, callback]);
+};
 
 export default function Navbar() {
-  const [showDropdown, setShowDropdown] = useState(false);
-  const [showNavbar, setShowNavbar] = useState(false);
-  const [navbarClasses, setNavbarClasses] = useState(styles.hide);
-  const dropdownRef = useRef<any>();
-  const testRef = useRef<any>();
-  const testRef2 = useRef<any>();
-  useOutsideAlerter(dropdownRef, HideDropdown);
+  const [showDropdown, setShowDropdown] = useState<boolean>(false);
+  const [showNavbarMobile, setShowNavbarMobile] = useState<boolean>(false);
+  const [navbarClasses, setNavbarClasses] = useState<string>(styles.hide);
+  const [searchInput, setSearchInput] = useState<string>('');
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
+  const dropdownMenuRef = useRef<HTMLLIElement>(null);
+  const navbarRef = useRef<HTMLUListElement>(null);
+  useOutsideClick(dropdownMenuRef, HideDropdown);
+  useOutsideClick(navbarRef, HideNavbarMobile);
 
   // Shows the navbar on the click of the menu. Used on smaller screens.
-  function ShowNavbar() {
-    setShowNavbar(() => true);
+  function ShowNavbarMobile(): void {
+    setShowNavbarMobile(() => true);
   }
 
-  function ShowNavbarDropdown() {
+  function SlideInNavbarMobile(): void {
     setNavbarClasses(styles.show);
   }
 
-  // TODO Run the HideNavbar function when user clicks outside of the navbar
-
   // Hides the navbar on the click of a x. Used on smaller screens.
-  function HideNavbar() {
-    setShowNavbar(() => false);
+  function HideNavbarMobile(): void {
+    setShowNavbarMobile(() => false);
+    if (showDropdown) setShowDropdown(() => false);
   }
 
-  function HideNavbarDropdown() {
+  function SlideOutNavbarMobile(): void {
     setNavbarClasses(styles.hide);
   }
 
-  // Hide or shows a dropdown menu on the click of the recipes link
-  function ToggleDropdown() {
-    setShowDropdown(!showDropdown);
+  function HideDropdown(): void {
+    setShowDropdown(() => false);
   }
 
-  function HideDropdown() {
-    setShowDropdown(false);
-  }
-
-  // Hides the dropdown menu if the user clicks anywhere on the screen
-  useEffect(() => {
-    function handleClickOutside(event: any) {
-      if (
-        testRef2.current &&
-        !testRef2.current.contains(event.target) &&
-        testRef.current &&
-        !testRef.current.contains(event.target)
-      ) {
-        HideNavbar();
-      }
-    }
-    // Bind the event listener
-    document.addEventListener('click', handleClickOutside);
-    return () => {
-      // Unbind the event listener on clean up
-      document.removeEventListener('click', handleClickOutside);
-    };
-  }, [testRef, testRef2]);
+  function HandleSearch(): void {}
 
   return (
     <>
@@ -92,30 +79,30 @@ export default function Navbar() {
           </div>
 
           <div
-            onClick={ShowNavbar}
+            onClick={ShowNavbarMobile}
             className={`${styles.toggle_btn} ${styles.mobile}`}
-            ref={testRef2}
           >
             <span></span>
             <span></span>
             <span></span>
           </div>
           <CSSTransition
-            in={showNavbar}
+            in={showNavbarMobile}
             timeout={1000}
             classNames={{
               enterActive: 'animate__fadeInRight',
               exitActive: 'animate__fadeOutRight',
             }}
-            onEnter={ShowNavbarDropdown}
-            onExited={HideNavbarDropdown}
+            onEnter={SlideInNavbarMobile}
+            onExited={SlideOutNavbarMobile}
             className={`animate__animated ${styles.navbar_links} ${navbarClasses}`}
           >
-            <ul ref={testRef}>
+            <ul ref={navbarRef}>
               <AiOutlineClose
-                onClick={HideNavbar}
+                onClick={HideNavbarMobile}
                 className={`${styles.close_btn} ${styles.mobile}`}
               />
+
               <li className={styles.website_name}>
                 <h2>
                   <Link
@@ -126,12 +113,12 @@ export default function Navbar() {
                   </Link>
                 </h2>
               </li>
-              <li className={styles.dropdown}>
+
+              {/* Recipes Tab Start */}
+              <li className={styles.dropdown} ref={dropdownMenuRef}>
                 <span
                   className={`${styles.link} ${styles.click_text}`}
-                  ref={dropdownRef}
-                  onClick={ToggleDropdown}
-                  // href='#'
+                  onClick={() => setShowDropdown(!showDropdown)}
                 >
                   Recipes
                 </span>
@@ -145,91 +132,118 @@ export default function Navbar() {
                     showDropdown == true ? styles.icon_arrow_up : styles.hide
                   }
                 />
-                <ul
-                  className={
-                    showDropdown == true
-                      ? `${styles.dropdown_content} animate__animated animate__fadeIn`
-                      : styles.hide
-                  }
-                >
-                  <li>
-                    <Link
-                      onClick={HideNavbar}
-                      className={styles.dropdown_link}
-                      to='/lunch-recipes'
-                    >
-                      Lunch
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      onClick={HideNavbar}
-                      className={styles.dropdown_link}
-                      to='/dinner-recipes'
-                    >
-                      Dinner
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      onClick={HideNavbar}
-                      className={styles.dropdown_link}
-                      to='/side-recipes'
-                    >
-                      Sides
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      onClick={HideNavbar}
-                      className={styles.dropdown_link}
-                      to='/dessert-recipes'
-                    >
-                      Dessert
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      onClick={HideNavbar}
-                      className={styles.dropdown_link}
-                      to='/all-recipes'
-                    >
-                      View All
-                    </Link>
-                  </li>
-                </ul>
+                {showDropdown && (
+                  <ul
+                    className={`${styles.dropdown_content} animate__animated animate__fadeIn`}
+                  >
+                    <li>
+                      <Link
+                        onClick={HideNavbarMobile}
+                        className={styles.dropdown_link}
+                        to='/lunch-recipes'
+                      >
+                        Lunch
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        onClick={HideNavbarMobile}
+                        className={styles.dropdown_link}
+                        to='/dinner-recipes'
+                      >
+                        Dinner
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        onClick={HideNavbarMobile}
+                        className={styles.dropdown_link}
+                        to='/side-recipes'
+                      >
+                        Sides
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        onClick={HideNavbarMobile}
+                        className={styles.dropdown_link}
+                        to='/dessert-recipes'
+                      >
+                        Dessert
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        onClick={HideNavbarMobile}
+                        className={styles.dropdown_link}
+                        to='/all-recipes'
+                      >
+                        View All
+                      </Link>
+                    </li>
+                  </ul>
+                )}
               </li>
+              {/* Recipes Tab End */}
+
               <li>
                 <Link
-                  onClick={HideNavbar}
-                  className={styles.link}
-                  to='/quick-recipes'
-                >
-                  30 Minutes Or Less
-                </Link>
-              </li>
-              <li>
-                <Link
-                  onClick={HideNavbar}
+                  onClick={HideNavbarMobile}
                   className={styles.link}
                   to='/favorites'
                 >
                   Favorites
                 </Link>
               </li>
-              <li>
-                <Link
-                  onClick={HideNavbar}
+
+              <li className={styles.search_bar}>
+                <div
+                  className={`${styles.search_bar_container} ${
+                    isExpanded ? styles.expanded : ''
+                  }`}
+                >
+                  <button
+                    className={styles.search_button}
+                    type='button'
+                    onClick={HandleSearch}
+                  >
+                    <AiOutlineSearch />
+                  </button>
+                  <input
+                    type='search'
+                    placeholder='Search for recipes here ex: beef'
+                    onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                      setSearchInput(e.target.value)
+                    }
+                    value={searchInput}
+                  />
+                  <button
+                    className={styles.close_button}
+                    type='button'
+                    onClick={() => setIsExpanded(false)}
+                  >
+                    <AiOutlineClose />
+                  </button>
+                </div>
+                <button
+                  className={isExpanded ? styles.hide : styles.search_icon}
+                  type='button'
+                  onClick={() => setIsExpanded(true)}
+                >
+                  <AiOutlineSearch />
+                </button>
+                {/* <Link
+                  onClick={HideNavbarMobile}
                   className={`${styles.link} ${styles.search}`}
                   to='/search'
                 >
                   <AiOutlineSearch />
-                </Link>
+                </Link> */}
               </li>
             </ul>
           </CSSTransition>
 
-          <hr />
+          {/* <hr /> */}
         </nav>
       </header>
     </>
